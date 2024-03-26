11775519405145790470112874379f551ba3f037
Madiha shah <162611745+madiha-sha@users.noreply.github.com>
2024-03-26 09:30:08 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+//assignment 4
+var famousQuote = "a person who never made a mistake never tried anything new.";
+var author = "albert einstein";
+console.log("".concat(author, " once said \"").concat(famousQuote, "\""));
+//assignment 5
+var famousPerson = "albert einstein";
+var message = ("".concat(famousPerson, "once said \"").concat(famousQuote));
+console.log("message");
---
+++ main.ts
@@ -0,0 +1,11 @@
+//assignment 4
+let famousQuote: string = "a person who never made a mistake never tried anything new.";
+let author: string = "albert einstein";
+
+console.log(`${author} once said "${famousQuote}"`);
+
+//assignment 5
+let famousPerson:string = "albert einstein";
+let message: string =(`${famousPerson}once said "${famousQuote}`);
+
+console.log("message");
